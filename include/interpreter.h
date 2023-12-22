a5c27137737da7d357ff7ec79ddf54627e797c47
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-12-22 14:01:31 +0200

    doc(interpreter): add comments

--- include/interpreter.h
+++ include/interpreter.h
@@ -8,10 +8,19 @@ using namespace std;
 
 class Interpreter
 {
+    // Load command: read html file and parse it DOM tree
     static void load(string filepath, DomTree *&tree);
+
+    // Print command: print the DOM tree
     static void print(DomTree *&tree);
+
+    // Selector command $(" "): Search for an element in the DOM tree
     static void search(string selector, DomTree *&tree);
+
+    // Search for an element by it id attribute
     static void searchById(string selector, DomTree *&tree);
+
+    // Help command: print the available commands in the program
     static void help();
 
 public:
